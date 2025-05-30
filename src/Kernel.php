25e5ba68af25190c4f88f1f2457e44804273f439
Egor Taranov <dev@taranovegor.com>
2025-05-30 18:16:31 +0200

    Initial project setup

---
+++ src/Kernel.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * © 2025 pet.restaurateur — Licensed under AGPL-3.0-or-later.
+ * See LICENSE file or https://www.gnu.org/licenses/agpl-3.0.html for details.
+ */
+
+namespace App;
+
+use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+
+class Kernel extends BaseKernel
+{
+    use MicroKernelTrait;
+}
